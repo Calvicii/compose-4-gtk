c6887e124e02ec15be7144de628d8f2ee4364e4a
Thomas Lavoie <lavoiethomas17@gmail.com>
2025-05-15 19:20:36 -0400

    Make header flat

--- src/test/kotlin/RadioButton.kt
+++ src/test/kotlin/RadioButton.kt
@@ -8,12 +8,14 @@ import io.github.compose4gtk.adw.components.HeaderBar
 import io.github.compose4gtk.adw.components.StatusPage
 import io.github.compose4gtk.gtk.components.RadioButton
 import io.github.compose4gtk.gtk.components.VerticalBox
+import io.github.compose4gtk.modifier.Modifier
+import io.github.compose4gtk.modifier.cssClasses
 
 fun main(args: Array<String>) {
     application("my.example.hello-app", args) {
         ApplicationWindow(title = "Radio Buttons", onClose = ::exitApplication) {
             VerticalBox {
-                HeaderBar()
+                HeaderBar(modifier = Modifier.cssClasses("flat"))
 
                 StatusPage(
                     title = "Radio Button", description = "Useful for selecting one option out of many"
